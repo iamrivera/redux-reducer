a3c8538100eccf6eb14c920768a2ce4eaadec328
iamrivera <a3m3rivera@gmail.com>
2021-09-14 12:51:26 -0700

    commit tree 4d970eef439785d1116e726c9573e2f6a1c2e008

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state={ friends: []}, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return { friends: [...state.friends.slice(0, action.index), action.friend] }
+        case "REMOVE_FRIEND":
+            return { friends: state.friends.filter(friend => friend.id !== action.id)}
+        default: 
+            return state;
+    }
 }
